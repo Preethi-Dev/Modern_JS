df2c6f509c7b00b423a1a153ed796125ff9ed261
Preethi <selvampreethi360@gmail.com>
2021-06-29 21:01:43 +0530

    day1 learning => modern keywords => let, const

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+//let is modern keyword to store values in variables
+let myAge = 22;
+myAge = 25;
+//while using const keyword we can't override that value
+const myName = "preethi_dev";
+//we can log using console.log
+console.log("my age is " + myAge +" my name is "+ myName);
