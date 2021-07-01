98d965a774fc4d4ce59fe049de802bfb5027f277
Preethi <selvampreethi360@gmail.com>
2021-07-01 11:51:40 +0530

    sting property &  method, Template Literals

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Modern_JS
 Learn modern JS from basic concepts 
-    - variables and constants
-    - data types
+    - string
+        => properties and methods
+    - numbers
+        => BIDMAS Rule applied
+        => +, -, /(division), **(indices), %(reminder)
 
+Short Hand => for mathematical operations
+    - num = num + 1 => num++ or num += 1
+    (Above method is similar to all Math Operations)
+
+we can't do math operation with string => results NaN(Not a Number)
+    -we can concatenate string with num using '+'
+    
 So, Let's get started
--- sandbox.js
+++ sandbox.js
@@ -10,14 +10,29 @@ let lastName = 'Dev';
 let fullName = firstName + ' ' + lastName;
 console.log(fullName);
 
-//getting character
+//searching character by index
 console.log(fullName[0]);
 
 //Properties - total length
 console.log(fullName.length);
 
 //method 
+//convert to uppercase
 console.log(fullName.toUpperCase());
-
+//convert to lowercase
 let example = fullName.toLowerCase();
 console.log(example, fullName);
+//find index using indexOf() method
+console.log("index of 'D' character: " + fullName.indexOf('D'));
+//find index using lastIndexOf() method
+console.log("index of 'i' character: " + fullName.lastIndexOf('i'));
+//slice(start index, end index) method => cut part of string
+console.log(fullName.slice(0,7));
+//substr(start index, count of character) method => fetch part of string
+console.log(fullName.substr(8,3));
+//repace(search value, replace value) method
+console.log(fullName.replace('e', 'E'));
+
+//template String or template literals
+//instead using '+' symbol for concatnate use template Literal to avoid messy code
+console.log(`my firstName is ${firstName} and my lastName is ${lastName}`);
