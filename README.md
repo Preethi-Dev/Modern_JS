0377870121c7a2956adf9419a64a31228a2b1274
Preethi <selvampreethi360@gmail.com>
2021-07-02 12:11:19 +0530

    loose & Strict Comparison and type conversion

--- README.md
+++ README.md
@@ -3,4 +3,16 @@ Learn modern JS from basic concepts
    Null and undefined
     -null => intentionally have no value (browser set default value => 'undefined')
     -undefined => unintentionally have no value  (browser set default value => 'null/zero')
+
+Loose and Strict Comparison
+    == => loose comparison(can't check data type) => implicit Type Conversion takes place
+    === => strict comparison(check data type) => implicit Type Conversion doesn't takes place
+
+Explicit Type Conversion
+    =>Convert to number 
+        -Number(variable name or value)
+    =>Convert to string
+        -String(variable name or value)
+    =>Convert to Boolean 
+        -Boolean(variable name or value)
 So, Let's get started
--- sandbox.js
+++ sandbox.js
@@ -7,4 +7,46 @@ console.log(age, age + 3, `the age is ${age}`);
 let year = null;
 //variable is assigned to null => then browser take variable contain no value
 //in any operation => null value acts as zero
-console.log(year, year + 3, `year is ${year}`); 
+console.log(year, year + 3, `year is ${year}`);
+
+//boolean comparison(==, !=, <, >, <=, >=)
+let number = 20;
+console.log(number != 25);//20 is not equal to 25
+
+
+//boolean comparison in String
+let fName = 'mahi';
+//compare by 1st letter of both string
+//'m' from mahi compare with 'z' from zebra
+//      => index position of 'm' is less than index position of 'z' in alphabt
+console.log(fName > 'zebra');
+// Uppercase letter is always lesser than LowerCase Letter
+console.log(fName > 'Zebra');
+
+
+//loose comparison => it deosn't check data type => incase we compare different data type, it did type conversion
+console.log(5 == '5'); //true
+console.log(5 != '5'); //false
+//strict comparison => it check data type => incase we compare different data type, it can't do type conversion
+console.log(5 === '5'); //false
+console.log(5 !== '5'); //true
+
+
+//Explicit Type conversion
+let score = '100';
+
+//convert to Number
+score = Number(score);
+console.log(score, typeof score);
+
+//Convert to String
+score = String(score);
+console.log(score, typeof score);
+
+//convert to Boolean
+//positive and negative number(minus symbol) => True
+//zero => False
+//all string values => True
+score = -100;
+score = Boolean(score);
+console.log(score, typeof score);
