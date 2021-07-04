05b75c90c06c05e0061259c0cc7a5a8fc4f808cd
Preethi <selvampreethi360@gmail.com>
2021-07-04 12:03:03 +0530

    control flow basics

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Modern_JS
 Learn modern JS from basic concepts 
-   Null and undefined
-    -null => intentionally have no value (browser set default value => 'undefined')
-    -undefined => unintentionally have no value  (browser set default value => 'null/zero')
-
-Loose and Strict Comparison
-    == => loose comparison(can't check data type) => implicit Type Conversion takes place
-    === => strict comparison(check data type) => implicit Type Conversion doesn't takes place
-
-Explicit Type Conversion
-    =>Convert to number 
-        -Number(variable name or value)
-    =>Convert to string
-        -String(variable name or value)
-    =>Convert to Boolean 
-        -Boolean(variable name or value)
+   Control Flow Basics
+    -looping statements
+        =>for loop
+        =>while loop
+        =>dowhile loop
+    -conditional statements
+        =>if statement
+        =>if-else statement
+        =>continue & break
+            -continue(skip the current iteration and go to next iteration)
+            -break(stop and come out of loop)
+        =>switch statement
+            - works like '===' matching
+    -Block scope
+        =>let
+        =>const
+    -Not Block Scope
+        =>var
 So, Let's get started
--- sandbox.js
+++ sandbox.js
@@ -1,52 +1,31 @@
-//undefined(not Intentionally)
-let age;
-//variable is not assigned to any value => then browser set default value as 'undefined'
-console.log(age, age + 3, `the age is ${age}`);
+//control flow basics
+//loop statement
+//for loop
+const names = ['jhon', 'michel', 'tom'];
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
 
-//null
-let year = null;
-//variable is assigned to null => then browser take variable contain no value
-//in any operation => null value acts as zero
-console.log(year, year + 3, `year is ${year}`);
+//while loop
+let count = 0;
+while(count < names.length){
+    console.log(names[count]);
+    count++;
+}
 
-//boolean comparison(==, !=, <, >, <=, >=)
-let number = 20;
-console.log(number != 25);//20 is not equal to 25
+//do while loop
+let iteration = 3;
+do{
+    console.log(iteration);
+    iteration++;
+}while(iteration < names.length);
 
-
-//boolean comparison in String
-let fName = 'mahi';
-//compare by 1st letter of both string
-//'m' from mahi compare with 'z' from zebra
-//      => index position of 'm' is less than index position of 'z' in alphabt
-console.log(fName > 'zebra');
-// Uppercase letter is always lesser than LowerCase Letter
-console.log(fName > 'Zebra');
-
-
-//loose comparison => it deosn't check data type => incase we compare different data type, it did type conversion
-console.log(5 == '5'); //true
-console.log(5 != '5'); //false
-//strict comparison => it check data type => incase we compare different data type, it can't do type conversion
-console.log(5 === '5'); //false
-console.log(5 !== '5'); //true
-
-
-//Explicit Type conversion
-let score = '100';
-
-//convert to Number
-score = Number(score);
-console.log(score, typeof score);
-
-//Convert to String
-score = String(score);
-console.log(score, typeof score);
-
-//convert to Boolean
-//positive and negative number(minus symbol) => True
-//zero => False
-//all string values => True
-score = -100;
-score = Boolean(score);
-console.log(score, typeof score);
+//block scope
+//let and const are block scope variable
+let score = 10;
+//conditional statement
+if(true){
+   let score = 20; //accessible within the code block and can't access outside code block
+    console.log('inside scope: ', score);
+}
+console.log('outside scope: ', score);
