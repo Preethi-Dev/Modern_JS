519a0e8f7c2e2e2d348e66c1c03441f4e559436f
Preethi <selvampreethi360@gmail.com>
2021-07-07 16:27:25 +0530

    remove some code

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,4 @@
 //object literal notation
-// let user = {
-//     name: 'mike', //property
-//     age: 30, //property
-//     location: 'europe', //property
-//     blogs: ['use old vs new variable', '100 days python journey'],
-//     login: function(){ //method
-//         console.log('login successfully');
-//     }, 
-//     logout: function(){ //method
-//         console.log('logout successfully');
-//     },
-//     logBlogs: function(){ //use normal function for 'this' keyword
-//         this.blogs.forEach((item, index) => {
-//             console.log(`Blog ${index} is ${item}`);
-//         })
-//     }
-// };
 let user = {
     name: 'mike', //property
     age: 30, //property
