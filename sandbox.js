c4ccab802c8d6adba4f4fd46544ec0cc31cf90a7
Preethi <selvampreethi360@gmail.com>
2021-07-07 16:06:37 +0530

    objects in array

--- sandbox.js
+++ sandbox.js
@@ -21,6 +21,10 @@ let user = {
     age: 30, //property
     location: 'europe', //property
     blogs: ['use old vs new variable', '100 days python journey'],
+    projects: [
+        {title: 'Home automation', period: '3 month'},
+        {title: 'Automate portal', period: '2 week'}
+    ],
     login(){ //method => we can rewrite it like login => function(){} ===> login(){}
         console.log('login successfully');
     }, 
@@ -31,7 +35,13 @@ let user = {
         this.blogs.forEach((item, index) => {
             console.log(`Blog ${index} is ${item}`);
         })
+    },
+    logProjects: function(){
+        this.projects.forEach((project, count) => {
+            console.log(`project ${count} is ${project.title} take ${project.period} time`);
+        })
     }
 };
+user.logProjects();
 user.logBlogs();
 console.log(this); //windows object => global object
