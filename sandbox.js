04cc81b8302ae9d77d1b28f27ddaf6875397216e
Preethi <selvampreethi360@gmail.com>
2021-07-07 15:45:01 +0530

    rewrite object methods

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,33 @@
 //object literal notation
+// let user = {
+//     name: 'mike', //property
+//     age: 30, //property
+//     location: 'europe', //property
+//     blogs: ['use old vs new variable', '100 days python journey'],
+//     login: function(){ //method
+//         console.log('login successfully');
+//     }, 
+//     logout: function(){ //method
+//         console.log('logout successfully');
+//     },
+//     logBlogs: function(){ //use normal function for 'this' keyword
+//         this.blogs.forEach((item, index) => {
+//             console.log(`Blog ${index} is ${item}`);
+//         })
+//     }
+// };
 let user = {
     name: 'mike', //property
     age: 30, //property
     location: 'europe', //property
     blogs: ['use old vs new variable', '100 days python journey'],
-    login: function(){ //method
+    login(){ //method => we can rewrite it like login => function(){} ===> login(){}
         console.log('login successfully');
     }, 
-    logout: function(){ //method
+    logout(){ //method
         console.log('logout successfully');
     },
-    logBlogs: function(){ //use normal function for 'this' keyword
+    logBlogs(){ //use normal function for 'this' keyword
         this.blogs.forEach((item, index) => {
             console.log(`Blog ${index} is ${item}`);
         })
