522742dbdbc0572e1b52a1e98fb8783dd9f6fddb
Preethi <selvampreethi360@gmail.com>
2021-07-10 16:30:20 +0530

    parent,child,sibling relationship

--- sandbox.js
+++ sandbox.js
@@ -19,3 +19,22 @@ const content = document.querySelector('.content');
 content.innerHTML += '<h1> hello, ninja</h1>';
 const content1 = document.querySelector('body > div > h1');
 content1.innerText = 'hello, keekee';
+
+//parent,child,sibling relationship => traverse the DOM
+const bigger_selection = document.querySelector('article'); //HTMLcollection => so, we can't apply forEach method
+//convert HTMLcollection to array => in array, we can apply forEach method
+console.log(Array.from(bigger_selection.children)); //all child of parent element(article)
+Array.from(bigger_selection.children).forEach(child => {
+    child.classList.add('article-style');
+})
+
+//find parent element
+const child = document.querySelector('h2');
+console.log(child.parentElement);
+//find next sibling
+console.log(child.nextElementSibling);
+//find previous sibling
+console.log(child.previousElementSibling);
+
+//traversing the element
+console.log(child.nextElementSibling.parentElement.children);
