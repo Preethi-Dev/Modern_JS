f120cf6d0b8d3f91d90e0ca4d057a46f664a2929
Preethi <selvampreethi360@gmail.com>
2021-07-10 11:32:09 +0530

    get and set attribute

---
+++ dom.js
@@ -0,0 +1,6 @@
+//get attribute
+const get_attr = document.querySelector('#one'); //querying the element by id
+console.log(get_attr.getAttribute('id')); //get the attribute
+//set attribute
+get_attr.setAttribute('id', 'firstPara'); //change the value of exisiting attribute
+get_attr.setAttribute('style', 'color:gray'); //create new attribute
