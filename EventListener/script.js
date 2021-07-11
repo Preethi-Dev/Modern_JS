6fda1887f7d542a3fe274f723b09feb84d2ae333
Preethi <selvampreethi360@gmail.com>
2021-07-11 10:50:59 +0530

    form events

--- EventListener/script.js
+++
@@ -1,7 +0,0 @@
-const paras = document.querySelector('div');
-//HTMLcollection converted to array and apply forEach method
-Array.from(paras.children).forEach(para => {
-    para.addEventListener('click', element => { //add event listener for every element
-        para.innerHTML = `<del>${element.target.innerText}</del>`; //element.target means current element
-    });
-});
--- sandbox.js
+++ sandbox.js
@@ -1,40 +1,6 @@
-//querying the element => fetch the element
-const para = document.querySelector('p');
-console.log(para);
-//querying all the elements
-const paras = document.querySelectorAll('p'); //nodeList
-console.log(paras);
-paras.forEach(para => {
-    console.log(para);
+const form = document.querySelector('.webForm');
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); //prevent the default behaviour of browser
+    document.querySelector('strong').innerHTML = `<center>${document.querySelector('.username').value}</center>`;
+    document.querySelector('strong').setAttribute('style', 'font-size: 32px');
 });
-//get reference by id
-console.log(document.getElementById('one'));
-//get reference by class name
-console.log(document.getElementsByClassName('one')); //HTMLcollections => can't apply forEach
-//get reference by tagname
-console.log(document.getElementsByTagName('p'));
-
-//add html template
-const content = document.querySelector('.content');
-content.innerHTML += '<h1> hello, ninja</h1>';
-const content1 = document.querySelector('body > div > h1');
-content1.innerText = 'hello, keekee';
-
-//parent,child,sibling relationship => traverse the DOM
-const bigger_selection = document.querySelector('article'); //HTMLcollection => so, we can't apply forEach method
-//convert HTMLcollection to array => in array, we can apply forEach method
-console.log(Array.from(bigger_selection.children)); //all child of parent element(article)
-Array.from(bigger_selection.children).forEach(child => {
-    child.classList.add('article-style');
-})
-
-//find parent element
-const child = document.querySelector('h2');
-console.log(child.parentElement);
-//find next sibling
-console.log(child.nextElementSibling);
-//find previous sibling
-console.log(child.previousElementSibling);
-
-//traversing the element
-console.log(child.nextElementSibling.parentElement.children);
